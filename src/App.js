61bf5e0e532e19318da6a0dd0af577d058130ed0
abdallahSQaradeh <abdallahsqaradeh@gmail.com>
2020-12-21 10:29:16 +0200

    propTypes, react-icons, and fonts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
 // import cloneDeep from "lodash.clonedeep";
+import NavBar from "./components/header/header.component";
+import Footer from "./components/footer/footer.component";
+import Content from "./pages/home/home.page";
 
 function App() {
   // const example = cloneDeep({ ex: "ex" });
   // console.log(example);
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <NavBar />
+      <Content />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
