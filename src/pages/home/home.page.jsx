271aa9f7b4ade40523baf206d01452cbffde9d48
abdallahSQaradeh <abdallahsqaradeh@gmail.com>
2020-12-21 16:48:14 +0200

    reedit some style

--- src/pages/home/home.page.jsx
+++ src/pages/home/home.page.jsx
@@ -5,23 +5,24 @@ import data from "../../data/data.json";
 
 export default function Home() {
   const { bannerHeader, bannerText, bannerButton } = data.home;
+
   return (
     <div className="home-content">
       <div className="banner">
-        <div className="banner-header">
-          <span className="b-bold">{bannerHeader.bold1}</span>
-          <span>{bannerHeader.normal}</span>
-          <span className="b-bold">{bannerHeader.bold2}</span>
+        <div className="banner-info">
+          <div className="banner-header">
+            <span className="b-bold">{bannerHeader.bold1}</span>
+            <span>{bannerHeader.normal}</span>
+            <span className="b-bold">{bannerHeader.bold2}</span>
+          </div>
+          <div className="banner-text">{bannerText}</div>
+          <button type="button" className="banner-button">
+            {bannerButton}
+          </button>
         </div>
-        <div className="banner-text">{bannerText}</div>
-        <button type="button" className="banner-button">
-          {bannerButton}
-        </button>
-      </div>
-      <div className="banner-image">
-        <picture>
+        <div className="banner-image">
           <img src={Peca} alt="pecatchoo" />
-        </picture>
+        </div>
       </div>
     </div>
   );
