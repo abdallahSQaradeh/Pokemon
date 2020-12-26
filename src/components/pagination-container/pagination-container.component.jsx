7e480de36ea55eb408b433d6ee44e4d5eced690f
abdallahSQaradeh <abdallahsqaradeh@gmail.com>
2020-12-26 23:33:07 +0200

    add Modal feature to each pokemon

--- src/components/pagination-container/pagination-container.component.jsx
+++ src/components/pagination-container/pagination-container.component.jsx
@@ -17,16 +17,45 @@ export default function PaginationContainer(props) {
         {!loading &&
           data.map((pokemon) => {
             const name = pokemon.name.english;
-            const { Attack } = pokemon.base;
-            const { Defense } = pokemon.base;
+            const { Attack, Defense, HP } = pokemon.base;
+            const spDefense = pokemon.base["Sp. Attack"];
+            const spAttack = pokemon.base["Sp. Defense"];
+            let Ability = "";
+
+            if (pokemon.profile.ability.length > 0)
+              for (let i = 0; i < pokemon.profile.ability.length; i += 1) {
+                Ability += `${pokemon.profile.ability[i][0]}${
+                  i === pokemon.profile.ability.length - 1 ? "" : "-"
+                }`;
+              }
+            else Ability = "Weak";
+            let gen = 0;
+            if (pokemon.evolution.next) {
+              // eslint-disable-next-line prefer-destructuring
+              gen = pokemon.evolution.next[0][1];
+            } else if (pokemon.evolution.prev) {
+              // eslint-disable-next-line prefer-destructuring
+              gen = pokemon.evolution.prev[0][1];
+            } else {
+              gen = "Old";
+            }
+
             return (
               <SinglPokemon
+                generation={gen}
+                Ability={Ability}
+                Experience={pokemon.base.Speed}
+                HP={HP}
                 src={pokemon.thumbnail}
                 key={pokemon.id}
+                id={pokemon.id}
                 attack={Attack}
                 color={_data.pokedex.types[pokemon.type[0]]}
                 defense={Defense}
                 name={name}
+                spDefense={spDefense}
+                spAttack={spAttack}
+                srcModal={pokemon.hires}
               />
             );
           })}
