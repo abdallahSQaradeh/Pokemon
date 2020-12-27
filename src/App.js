123d139bcea5c198adb2c5539cea01521cfdb6bd
abdallahSQaradeh <abdallahsqaradeh@gmail.com>
2020-12-27 18:02:16 +0200

    enhance the picture in the home page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import "./App.css";
 // import cloneDeep from "lodash.clonedeep";
 import NavBar from "./components/navbar/navbar.component";
 import Footer from "./components/footer/footer.component";
-// import Content from "./pages/home/home.page";
+import Content from "./pages/home/home.page";
 // import ErrorPage from "./pages/E404/e404.components";
-import Pokedex from "./pages/pkedex/pokedex.component";
+// import Pokedex from "./pages/pkedex/pokedex.component";
 // import useFetchPokemon from "./hooks/useFetchPokemon";
 
 function App() {
@@ -14,9 +14,9 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <Pokedex />
-      {/*  <Content />
-      <ErrorPage /> */}
+      {/*  <Pokedex /> */}
+      <Content />
+      {/* <ErrorPage /> */}
       <Footer />
     </div>
   );
